0d60789e8e0fcb7a3cf574f954bf7548dd9264ca
HECHONG999 <1259796851@qq.com>
2020-05-07 23:39:18 +0800

    commit

--- index.js
+++ index.js
@@ -1,28 +1,9 @@
 const express = require("express");
-const globalConfig = require("./config.js");
-const loader = require("./loader.js")
+const loader = require("./loader");
+
 const app = new express();
 
-// app.post("/editEveryDay", loader.get("/editEveryDay"))
-// app.get("/queryEveryDay", loader.get("/queryEveryDay"));
-// app.post("/addBlog", loader.get("/addBlog"))
-
-// app.get("/queryBlogByPageSize", loader.get("/queryBlogByPageSize"));
-// app.get("/queryAllBlogCount", loader.get("/queryAllBlogCount"))
-
-// app.get("/getBlogDetail", loader.get("/getBlogDetail"));
-
-// app.get("/addComment", loader.get("/addComment"))
-// app.get("/queryRandomCode", loader.get("/queryRandomCode"));
-// app.get("/queryCommentsByBlogId", loader.get("/queryCommentsByBlogId"))
-// app.get("/queryCommentCount", loader.get("/queryCommentCount"))
-
-// app.get("/queryTagsAll", loader.get("/queryTagsAll"));
-// app.get("/queryHotBlog", loader.get("/queryHotBlog"));
-// app.get("/queryNewComment", loader.get("/queryNewComment"))
-
-app.use( express.static("./page/"))
-
-app.listen(globalConfig.port, function () {
-    console.log("服务已经启动")
+app.use(express.static("./page"))
+app.listen("12306", () => {
+    console.log("服务已经启动 ~~~~")
 })
--- utils/getNow.js
+++
@@ -1,8 +0,0 @@
-function getNow() {
-    return parseInt(new Date().getTime() / 1000) 
-}
-
-
-module.exports  = {
-    "getNow": getNow
-}
