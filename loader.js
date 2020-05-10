a0c2ad3995f25e97394bda645345aab520f4064e
HECHONG999 <1259796851@qq.com>
2020-05-10 09:18:56 +0800

    commit

--- loader.js
+++ loader.js
@@ -7,7 +7,6 @@ const fs = require("fs");
 const globalConfig = require("./config");
 let controllerSet = [];
 let map = new Map();        // Map数据结构:数据唯一特性 -- get和set方法获取或设置数据
-
 let dirs = fs.readdirSync("./web");  //readdirSync:同步读取文件的目录
 /**
  * 读取web文件,封装url请求数据的业务逻辑接口
@@ -17,10 +16,10 @@ function readFiles(dirs) {
     let fileArr;
     for(let i = 0; i < dirs.length; i ++) {
        try {
-        fileArr = require("./" + globalConfig["web_path"] +"/"+ dirs[i]); // { queryAllBlogCount: [Function: queryAllBlogCount] }
+        fileArr = require("./web"+"/"+ dirs[i]); // { queryAllBlogCount: [Function: queryAllBlogCount] }
        } catch(e) {
 
-           throw Error("loader File" + "文件读取错误" + e)
+           throw Error("loader File" + "file read error" + e)
        }
         for( let [key,value] of fileArr) {
             if(map.get(key) == null) {       // 如果map中没有此url地址，就添加该url的服务接口
